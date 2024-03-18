09be44ba452f41077738a0766cafa364c485ab4c
VIGNESH <148709718+Vignesharmi0826@users.noreply.github.com>
2024-03-19 03:46:48 +0530

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$subject = $_POST['subject'];
+	$mailFrom = $_POST['email'];
+	$message = $_POST['message'];
+	
+	$mailto = "lvicky7720@gmail.com";
+	$headers = "From: ".$mailFrom;
+	$txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
+	
+	mail($mailto, $subject, $txt, $headers);
+	header("Location: https://www.lvicky.org/contact.html/?mailsend");
+ }
