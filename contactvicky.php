8363a043d0273b07107f201d7114ad0b0bd49a5e
VIGNESH <148709718+Vignesharmi0826@users.noreply.github.com>
2024-03-19 15:50:51 +0530

    Add files via upload

---
+++ contactvicky.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$subject = $_POST['subject'];
+	$mailFrom = $_POST['email'];
+	$message = $_POST['message'];
+	
+	$mailto = "lvicky8520@gmail.com";
+	$headers = "From: ".$mailFrom;
+	$txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
+	
+	mail($mailto, $subject, $txt, $headers);
+	header("Location: https://github.com/Vignesharmi0826/pizza-hunt-19-03-24/blob/main/contact.html?mailsend");
+ }
